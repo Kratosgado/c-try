7562c39442bf55e25b92217a1ce81187b7d2aa07
mbeah essilfie <mbeahessilfieprince@gmail.com>
2022-06-20 08:21:46 -0600

    Update and rename helloworld.cpp to low_calculator.cpp
    low functioning calculator using switch case

--- low_calculator.cpp
+++ low_calculator.cpp
@@ -3,11 +3,15 @@
 using namespace std;
 
 int main(){
+    //create a character that will be used to control whether to stop or continue calculating
     char contin = 'y';
+    //create constants that will be used in the switch cases
     const int ADD = 1, SUBTRACT = 2, MULTIPLY = 3, DIVIDE = 4, MODULUS = 5;
+    //create variables for our calculations
     int choice, first_number, second_number; double result;
     do
     {
+        //display the menu to the screen
          cout<<"Menu:\n\t1. Add\n\t2. Subtract\n\t3. Multiply\n\t4. Divide\n\t5. Modulus\nEnter your choice: ";
     cin>>choice;
     cout<<"Enter your two numbers separated by space: ";
@@ -34,7 +38,9 @@ int main(){
             cout<<"You entered a wrong option";
             break;
         }
+       //display the results
     cout<<result;
+        //puase the console
     system("PAUSE");
     cout<<"continue?(y/n): ";
     cin>>contin;
