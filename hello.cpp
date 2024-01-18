be2c2351fcc6c74e3ca4b38596ebe8a4921cf3c9
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2024-01-18 17:23:23 +0000

    edit task config to delete executable after debug

---
+++ hello.cpp
@@ -0,0 +1,13 @@
+// A simple c++ program
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   cout << "Programming is great fun!";
+   int x;
+   cout << "Enter your age: ";
+   cin >> x;
+
+   return 0;
+}
