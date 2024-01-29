61f2989324dad3501be157bb8a6b9638ee21280e
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2024-01-29 18:18:59 +0000

    start recursion

---
+++ recursion/counting.cpp
@@ -0,0 +1,22 @@
+// this program demonstrates a recursive function for counting the number of times a character appears in a string
+#include<iostream>
+#include <string>
+using namespace std;
+
+
+int numChars(char, string, int); // prototype
+
+int main() {
+   string str = "abcddddef";
+
+   cout << "The letter d appears "
+      << numChars('d', str, 0) << " timmes.\n";
+   return 0;
+}
+
+int numChars(char search, string str, int subscript) {
+   if (subscript >= str.length()) return 0;
+   else if (str[subscript] == search)
+      return 1 + numChars(search, str, subscript + 1);
+   else return numChars(search, str, subscript + 1);
+}
---
+++ recursion/factorial.cpp
@@ -0,0 +1,22 @@
+// // this program demonstrates a recursive function to calculate the factorial of a number
+// #include <iostream>
+// using namespace std;
+
+// int factorial(int);// prototype
+
+// int main() {
+//    int number;
+//    // Get a number from the user.
+//    cout << "Enter an integer value and I will display\n";
+//    cout << "its factorial: ";
+//    cin >> number;
+//    // Display the factorial of the number.
+//    cout << "The factorial of " << number << " is ";
+//    cout << factorial(number) << endl;
+//    return 0;
+// }
+
+// int factorial(int n) {
+//    if (n == 0) return 1;
+//    return n * factorial(n - 1);
+// }
